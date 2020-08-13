55f42d688a10c8b3bc740a15e0c924505ef09825
Asad <masa0002@student.monash.edu>
2020-08-13 14:42:44 +1000

    Added Google maps plugin

--- MainProject/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MainProject/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,155 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    SupportMapFragment smf;
+    FusedLocationProviderClient client;
+    Location location1;
+
+    GoogleMap googleMap1;
+    boolean haveInput;
+    String targetAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        smf = (SupportMapFragment) getSupportFragmentManager().
+                findFragmentById(R.id.google_map);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        //EditText editAddress  = (EditText) findViewById(R.id.address_edit);
+        //targetAddress = editAddress.getText().toString();
+
+        if (ActivityCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            /*if (!targetAddress.isEmpty()) {
+                getLocationFromAddress(this, targetAddress);
+            } else {
+                getCurrentLocation();
+            }*/
+            getCurrentLocation();
+//            updateMap();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+        }
+    }
+
+    private void getCurrentLocation() {
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                location1 = location;
+                if (location1 != null) {
+                    smf.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            LatLng latLing = new LatLng(location1.getLatitude(), location1.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLing).title("Target");
+
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLing, 10));
+
+                            googleMap.addMarker(options);
+
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    /*public void updateMap(){
+        LatLng latLing = getLocationFromAddress(this,targetAddress);
+        MarkerOptions options = new MarkerOptions().position(latLing).title("Target");
+
+        googleMap1.animateCamera(CameraUpdateFactory.newLatLngZoom(latLing, 10));
+
+        googleMap1.addMarker(options);
+    }*/
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, pemissions, grantResults);
+        if (requestCode == 44){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            } /*else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+            }*/
+        }
+    }
+
+
+
+
+    public LatLng getLocationFromAddress(Context context, String strAddress){
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return p1;
+    }
+
+    public void addressButtonOnCick(View view){
+    }
+
+    /*public void addressButtonOnCick(View view){
+        EditText editAddress  = (EditText) findViewById(R.id.address_edit);
+        targetAddress = editAddress.getText().toString();
+        haveInput = true;
+        //updateMap();
+    }*/
+
+
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -28,10 +159,10 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
-    }
+        });*/
+    //}
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -51,5 +182,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
